4e0fca640d6d17c51659a6d7d223d1b15c5a4cdc
cparragatorres <humbertpt14@gmail.com>
2022-05-19 16:28:56 -0500

    Mi cuarto Commit

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class MandarinaApp extends StatelessWidget {
               "Christian Parraga",
               style: TextStyle(
                 color: Colors.white,
-                fontSize: 24.0,
+                fontSize: 25.0,
                 fontFamily: "TitanOne Regular",
               ),
             ),
